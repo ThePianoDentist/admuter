af5e9a788a6bbad44f56fd00e87a3ee1f5a0de68
ThePianoDentist <jbknight07@gmail.com>
2017-03-09 04:18:15 +0000

    Hide twitch ad not remove

--- admuter/js/twitch_mute.js
+++ admuter/js/twitch_mute.js
@@ -1,11 +1,19 @@
 function checkAdTwitch(){
+    // Hard to get it to skip ad with button click
+    // because skip ad button is in iframe. cant work out how to access
+    // turns out you can just delete the iframe and the ad stops lol
+    // But feels really hacky and id rather avoid if possible
+    // current best solution is mute ad and hide the frame
+    var skip = true;
     var should_click = false;
     var mutedBtn = $(".mute-button");
     var muted = $(".player-volume__slider").find("div").css("width") == "0px";
-    var ad_on = $("#player").attr("data-screen") == "advertisement"; // always exists. but text empty when no add
-    var can_skip_btn = $(".videoAdUiSkipButton");
-    if (can_skip_btn.length > 0){
-        can_skip_btn.click();
+    var player = $("#player");
+    var ad_on = player.attr("data-screen") == "advertisement"; // always exists. but text empty when no add
+    //var can_skip_btn = $(".videoAdUiSkipButton"); cannot find because inside iframe (cross content issues)
+    if (ad_on && skip){
+        $(".player-video").find("iframe").hide()
+        //can_skip_btn.click();
     }
     if (ad_on && !muted){
         should_click = true;
