ff522b11f041084039c0d408e8f69d663a448e89
ThePianoDentist <jbknight07@gmail.com>
2017-03-09 00:13:37 +0000

    Trying out mutation observers/arrivejs. havent tested

--- admuter/js/muter.js
+++ admuter/js/muter.js
@@ -1,8 +1,18 @@
 function checkAd(){
-    var should_click = false;
+    var muted = false;
     var mutedBtn = $(".volume-bar").find("button");
-    var muted = (mutedBtn.attr("class").indexOf("off") !== -1);
-    var anchors = $(".now-playing-bar").find("a").slice(0, 2);
+    $(".now-playing-bar").arrive("a", function() {
+        var target = $(this).attr("target");
+        if (typeof target !== typeof undefined && target !== fals    e && !muted){
+            mutedBtn.click();
+            muted = !muted;
+        }
+    });
+    //var muted = false;
+    //var should_click = false;
+    //var mutedBtn = $(".volume-bar").find("button");
+    //var muted = (mutedBtn.attr("class").indexOf("off") !== -1);
+    /*var anchors = $(".now-playing-bar").find("a").slice(0, 2);
     anchors.each(function(){
         var target = $(this).attr("target");
         if (typeof target !== typeof undefined && target !== false && !muted){
@@ -11,13 +21,13 @@ function checkAd(){
         else if ((typeof target === typeof undefined || target === true) && muted){
         should_click = true;
         }
-    });
+    });*/
 
-    if (should_click){
+    /*if (should_click){
         console.log("Clicking mute Btn");
         mutedBtn.click();
     }
-    setTimeout(function(){checkAd();}, 1000);
+    setTimeout(function(){checkAd();}, 1000);*/
 }
 
 checkAd();
