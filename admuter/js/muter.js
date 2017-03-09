fa2f98f28c67c28aa39fbe34845ca64d7e2c433d
ThePianoDentist <jbknight07@gmail.com>
2017-03-09 00:13:49 +0000

    Revert "Trying out mutation observers/arrivejs. havent tested"
    This reverts commit ff522b11f041084039c0d408e8f69d663a448e89.

--- admuter/js/muter.js
+++ admuter/js/muter.js
@@ -1,18 +1,8 @@
 function checkAd(){
-    var muted = false;
+    var should_click = false;
     var mutedBtn = $(".volume-bar").find("button");
-    $(".now-playing-bar").arrive("a", function() {
-        var target = $(this).attr("target");
-        if (typeof target !== typeof undefined && target !== fals    e && !muted){
-            mutedBtn.click();
-            muted = !muted;
-        }
-    });
-    //var muted = false;
-    //var should_click = false;
-    //var mutedBtn = $(".volume-bar").find("button");
-    //var muted = (mutedBtn.attr("class").indexOf("off") !== -1);
-    /*var anchors = $(".now-playing-bar").find("a").slice(0, 2);
+    var muted = (mutedBtn.attr("class").indexOf("off") !== -1);
+    var anchors = $(".now-playing-bar").find("a").slice(0, 2);
     anchors.each(function(){
         var target = $(this).attr("target");
         if (typeof target !== typeof undefined && target !== false && !muted){
@@ -21,13 +11,13 @@ function checkAd(){
         else if ((typeof target === typeof undefined || target === true) && muted){
         should_click = true;
         }
-    });*/
+    });
 
-    /*if (should_click){
+    if (should_click){
         console.log("Clicking mute Btn");
         mutedBtn.click();
     }
-    setTimeout(function(){checkAd();}, 1000);*/
+    setTimeout(function(){checkAd();}, 1000);
 }
 
 checkAd();
